aa598da5976d45270f76393c7c7d798a02bb2cc0
ilya-uzun <ilya.uzun@bk.ru>
2022-08-28 10:17:25 +0500

    Изменил структуры программы

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from tools.compiler import Compiler
+from UI.ui import *
 
 
-class MainWindow(QMainWindow, Compiler):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        #self.add_function()
 
+    def setupUi(self, MainWindow):
+        super().setupUi(MainWindow)
+        MainWindow.setObjectName("MainWindow2")
+
+        self.lineEditLeft()
+        self.compoler = Compiler()
+
+    def lineEditLeft(self):
+        if self.lineEditConversionLeft.text() == None:
+            self.lineEditConversionLeft.editingFinished.connect(self.onChanged)
+
+
+    def onChanged(self):
+        self.lineEditConversionRiqht.setText(self.compoler.psi_in_Pa(self.lineEditConversionLeft.text()))
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
     mw = MainWindow()
     mw.show()
-
     sys.exit(app.exec_())
--- tools/compiler.py
+++ tools/compiler.py
@@ -1,17 +1,12 @@
 from data.dataCompiler import DadaCompiler
-from UI.ui import *
 
 
-class Compiler(Ui_MainWindow):
+class Compiler:
 
-    def setupUi(self, mainWindow):
-        super().setupUi(mainWindow)
-        mainWindow.setObjectName("MainWindow2")
-        self.lineEditConversionLeft.editingFinished.connect(self.onChanged)
+    def testText(self):
+        return 'test'
 
-    def psi_in_Pa(self):
-        return str(float(self.lineEditConversionLeft.text()) * DadaCompiler.PSI_IN_PA)
+    def psi_in_Pa(self, left: str):
+        return str(float(left) * DadaCompiler.PSI_IN_PA)
 
-    def onChanged(self):
-        self.lineEditConversionRiqht.setText(self.psi_in_Pa())
 
